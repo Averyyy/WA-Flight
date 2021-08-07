b7165dcee44e10ef624a29837f0d7cbd6c344648
wendy <ys3979@nyu.edu>
2021-08-08 00:48:40 +0800

    'skugi

---
+++ APP.py
@@ -0,0 +1,137 @@
+import pymysql
+# import hashlib
+import datetime
+import time
+from flask import Flask, render_template, request, session, redirect, url_for
+import Query_Utility as query
+import pprint
+
+
+app = Flask(__name__)
+app.secret_key = "NK3K"
+conn = pymysql.connect(host= '127.0.0.1',
+                       user='Wendy',
+                       password = '12345',
+                       port = 3307,
+                       db='Project',
+                       charset='utf8mb4',
+                       cursorclass=pymysql.cursors.DictCursor)
+
+@app.route('/', methods = ["GET","POST"])
+def init_app():
+    return render_template("index.html")
+
+@app.route('/public_view', methods = ["GET","POST"])
+def public_view():
+    if request.method =='GET':
+        data_dic = query.public_view(conn)
+        locations = query.get_locations(conn)
+        # pprint.pprint(data['arrival_city'])
+        # print(request.form['from'])
+        # print(data_dic)
+        print(locations)
+        return render_template("public_view.html",
+                           departure_city = locations['departure_loc'],
+                           arrival_city = locations['arrival_loc'],
+                           d = data_dic)
+
+    elif request.method == 'POST':
+        locations = query.get_locations(conn)
+        print(request.form['from'],
+              request.form['to'],
+              request.form['date'])
+        html_get = {'from':request.form['from'],
+                    'to':request.form['to'],
+                    'dt':request.form['date']}
+
+        print(request.form['from'])
+        print(1, html_get)
+        data_dic = query.filter_result(conn,html_get)
+        return render_template("public_view.html",
+                               departure_city=locations['departure_loc'],
+                               arrival_city=locations['arrival_loc'],
+                               d=data_dic)
+
+
+@app.route('/search',methods = ['GET','POST'])
+def ty():
+    print('Hello world')
+    return
+
+@app.route("/sign_up", methods=["POST", "GET"])
+def sign_up():
+
+    return render_template("signup.html")
+
+@app.route("/sign_in", methods=["POST", "GET"])
+def sign_in():
+    pass
+
+
+@app.route('/register/customer', methods=['GET', 'POST'])
+def signup_cus(identity):
+    error = ""
+    if request.method == "GET":
+        return render_template("signup.html", error=error)
+    elif request.method == "POST":
+        info = {"email": request.form.get("Email"),
+                "password": request.form.get("psw"),
+                "name": request.form.get("pre name"),
+
+                "building_number": request.form.get("bui num"),
+                "street": request.form.get("street"),
+                "city": request.form.get("city"),
+                "state": request.form.get("state"),
+                "phone_number": request.form.get("pho num"),
+                "passport_number": request.form.get("psp num"),
+                "passport_expiration": request.form.get("psp exp"),
+                "passport_country": request.form.get("psp cou"),
+                "date_of_birth": request.form.get("DoB"),
+                "booking_agent_id": request.form.get("booking_agent_id"),
+                "airline_name": request.form.get("airline_name")
+                }
+
+        valid, error = query.reg_validation(conn, info)
+        # when db find it is an invalid login
+        if not valid:
+            return render_template("signup.html", error=error)
+
+        db_utils.register_to_database(conn, info, identity)
+        session["logged_in"] = True
+        session["type"] = identity
+        session["email"] = info["email"]
+        if identity == "airline_staff":
+            session["airline"] = info["airline_name"]
+        return redirect(url_for("search_flight"))
+
+@app.route('/register/booking_agent', methods=['GET', 'POST'])
+def signup_ba(identity):
+    info = {
+        "email": request.form.get("username"),
+        "password": request.form.get("password"),
+        "name": request.form.get("name"),
+    }
+    
+
+
+@app.route('/register/airline_staff', methods=['GET', 'POST'])
+def signup_as(identity):
+
+    info = {
+        "email": request.form.get("username"),
+        "password": request.form.get("password"),
+        "first_name": request.form.get("first_name"),
+        "last_name": request.form.get("last_name"),
+        "date_of_birth": request.form.get("DoB"),
+        "airline_name": request.form.get("airline_name")
+    }
+
+
+
+
+
+
+
+
+if __name__ =='__main__':
+    app.run()
---
+++ Query_Utility.py
@@ -0,0 +1,119 @@
+import pymysql
+# import hashlib
+import datetime
+import time
+from flask import Flask, render_template, request, session, redirect, url_for
+import pprint
+# =================================
+dic_airport_city = {"PVG":"Shanghai",
+                    "PEK":"Beijing",
+                    "CAN":"Guangzhou",
+                    "SZX":"Shenzhen",
+                    "NRT":"Tokyo",
+                    "JFK":"New York",
+                    "LHR":"London"}
+
+def airport_city(airport):
+    return dic_airport_city[airport]
+
+def remove_duplicate(lis):
+    return list(set(lis))
+# =================================
+# the options for searching filter
+def get_locations(conn):
+    # Fetch all the data
+    cursor = conn.cursor()
+    query = "select * from flight"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+
+    d_dic = {
+        'departure_airport': [],
+        'arrival_airport': [],
+        'departure_loc':[],
+        'arrival_loc':[]
+    }
+
+    for i in range(len(data)):
+        d_dic['departure_airport'].append(str(data[i]['departure_name']))
+        d_dic['arrival_airport'].append(str(data[i]['arrival_name']))
+
+    # get rid of all the duplicate elements
+    d_dic['departure_airport'] = remove_duplicate(d_dic['departure_airport'])
+    d_dic['arrival_airport'] = remove_duplicate(d_dic['arrival_airport'])
+
+    # mapping back the airport to the city
+    for i in d_dic['departure_airport']:
+        d_dic['departure_loc'].append("%s | %s" % (airport_city(i), i))
+
+    for i in d_dic['arrival_airport']:
+        d_dic['arrival_loc'].append("%s | %s" % (airport_city(i), i))
+    print('/////////',d_dic)
+
+    return d_dic
+
+# the options for searching filter
+# NOT IN USE
+def get_departure_time():
+    d_dic = {
+        'departure_time': [],
+        'arrival_time': []
+    }
+
+    for i in range(len(data)):
+        d_dic['departure_time'].append(str(data[i]['departure_time']))
+        d_dic['arrival_time'].append(str(data[i]['arrival_time']))
+
+    # get rid of all the duplicate elements
+    d_dic['departure_time'] = remove_duplicate(d_dic['CHANGE'])
+    d_dic['arrival_time'] = remove_duplicate(d_dic['CHANGE'])
+
+
+
+def public_view(conn):
+    # From query fetch all
+    cursor=conn.cursor()
+    query = "select * from flight"
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+
+    for i in data:
+        i['Departure']= "%s | %s" % (airport_city(i['departure_name']),i['departure_name'])
+        i['Arrival']= "%s | %s" % (airport_city(i['arrival_name']),i['arrival_name'])
+    return data
+
+def filter_result(conn,html_get):
+    # html_get is a list from user filter input, it can have multiple results or it can be empty
+    # in the Flask html_get = {'from':request.form['from'],
+    #                     'to':request.form['to'],
+    #                     'dt':request.form['departure']} -- Shanghai | PVG
+    query = "select * from flight where"
+    if html_get['from']:
+        html_get['departure_name'] = html_get['from'].split('|')[1].strip()
+        query += ' %s = \'%s\' '% ('departure_name', html_get['departure_name'])
+    if html_get['to']:
+        html_get['arrival_name'] = html_get['to'].split('|')[1].strip()
+        query += 'and %s =\'%s\' ' % ('arrival_name', html_get['arrival_name'])
+    if html_get['dt'] != '':
+        query += 'and %s = \'%s\'' % ('dt', html_get['dt'])
+    cursor = conn.cursor()
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    for i in data:
+        i['Departure']= "%s | %s" % (airport_city(i['departure_name']),i['departure_name'])
+        i['Arrival']= "%s | %s" % (airport_city(i['arrival_name']),i['arrival_name'])
+    return data
+
+def reg_validation_cus(conn,info):
+
+    return status, err
+
+def reg_validation_cus(conn,info):
+
+    return valid, err
+
+def reg_validation_cus(conn,info):
+    return status, err
