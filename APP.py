23ef00a3a828d795b3b60ca4339d434647a8dd5f
Averyyy <58547284+Averyyy@users.noreply.github.com>
2021-08-12 21:05:48 +0800

    final version, including all the bug fixed

--- APP.py
+++ APP.py
@@ -1,10 +1,7 @@
 import pymysql
-# import hashlib
-import datetime
-import time
 from flask import Flask, render_template, request, session, redirect, url_for
 import Query_Utility as query
-import pprint
+# from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -38,13 +35,8 @@ def init_app():
 @app.route('/public_view', methods = ["GET","POST"])
 def public_view():
     if request.method =='GET':
-
         data_dic = query.public_view(conn)
         locations = query.get_locations(conn)
-        # pprint.pprint(data['arrival_city'])
-        # print(request.form['from'])
-        print(data_dic)
-        print(locations)
         return render_template("public_view.html",
                            departure_city = locations['departure_loc'],
                            arrival_city = locations['arrival_loc'],
@@ -52,15 +44,9 @@ def public_view():
 
     elif request.method == 'POST':
         locations = query.get_locations(conn)
-        print(request.form['from'],
-              request.form['to'],
-              request.form['date'])
         html_get = {'from':request.form['from'],
                     'to':request.form['to'],
                     'dt':request.form['date']}
-
-        print(request.form['from'])
-        print(1, html_get)
         data_dic = query.filter_result(conn,html_get)
         return render_template("public_view.html",
                                departure_city=locations['departure_loc'],
@@ -69,16 +55,14 @@ def public_view():
 
 
 # ====== sign up
-
 @app.route("/sign_up", methods=["POST", "GET"])
 def sign_up():
     if request.method =='GET':
-        print('yes')
         return render_template("signup.html")
     if request.method == 'POST':
         info_cus = {
             "email": request.form.get("Email"),
-            "password": request.form.get("psw"),
+            "password":  request.form.get("psw"),
             "name": request.form.get("pre name"),
             "building_number": request.form.get("bui num"),
             "street": request.form.get("street"),
@@ -96,10 +80,10 @@ def sign_up():
             "booking_agent_id": request.form.get("ba_id")
         }
         info_as = {
-            "username": request.form.get("as_uname"),
-            "password": request.form.get("psw"),
-            "first_name": request.form.get("first_name"),
-            "last_name": request.form.get("last_name"),
+            "email": request.form.get("as_uname"),
+            "password": request.form.get("as_psw"),
+            "first_name": request.form.get("as_first_name"),
+            "last_name": request.form.get("as_last_name"),
             "date_of_birth": request.form.get("asDoB"),
             "airline_name": request.form.get("as_airline")
         }
@@ -212,7 +196,6 @@ def customer_home():
                     'to': request.form.get('to'),
                     'dt': request.form.get('date'),
                     'flight_num':request.form.get("flight_num")}
-        print(html_get)
         data_dic = query.filter_result(conn, html_get)
         purchased_flight = query.get_purchased_flight(conn, session)
 
@@ -227,14 +210,12 @@ def customer_home():
                                airlines = query.get_airlines(conn),
                                flight_num = query.get_flight_num(conn),
                                data_list = d)
-
         else:
-
-            success,err = query.purchase(conn, flight_num, session['email'])
+            success,err = query.purchase(conn, flight_num, session['email'], 'NULL')
             if success:
-                return redirect(url_for("init_app"))
+                return redirect(url_for("customer_home"))
             else:
-                return redirect(url_for('sign_up'))
+                return redirect(url_for("customer_home"))
 
 
 @app.route("/sign_in/agent_home", methods=["POST", "GET"])
@@ -242,6 +223,11 @@ def agent_home():
     session['signin'] = query.sign_in_check(conn, session['email'],session["password"], 'booking_agent','')
     session["user_type"] = 'booking_agent'
     d = query.get_top_customer_number(conn,session)
+    locations = query.get_locations(conn)
+    total_month = query.view_commission_month(conn, session)[0]
+    avg_month = query.view_commission_month(conn, session)[1]
+    total_year = query.view_commission_month(conn, session)[2]
+    avg_year = query.view_commission_month(conn, session)[3]
     if not session["signin"] and request.method == 'GET':
         session["error"] = 'Invalid username or password, please try again.'
         return redirect(url_for("sign_in"))
@@ -249,12 +235,7 @@ def agent_home():
         data_dic = query.public_view(conn)
         locations = query.get_locations(conn)
         purchased_flight = query.get_purchased_flight(conn, session)
-        total_month = query.view_commission_month(conn, session)[0]
-        avg_month = query.view_commission_month(conn, session)[1]
-        total_year = query.view_commission_month(conn, session)[2]
-        avg_year = query.view_commission_month(conn, session)[3]
         return render_template('homepage_booking_agent.html',
-                               # same results as
                                departure_city=locations['departure_loc'],
                                arrival_city=locations['arrival_loc'],
                                all=data_dic,
@@ -264,6 +245,39 @@ def agent_home():
                                total_year= total_year,
                                avg_year=avg_year,
                                date_list = d)
+
+    elif request.method == 'POST':
+        html_get = {'from': request.form.get('from'),
+                    'to': request.form.get('to'),
+                    'dt': request.form.get('date'),
+                    'customer_email': request.form.get('customer_email'),
+                    'flight_num': request.form.get("flight_num")
+                    }
+        data_dic = query.filter_result(conn, html_get)
+        purchased_flight = query.get_purchased_flight(conn, session)
+        flight_num = html_get["flight_num"]
+        if flight_num == '':
+            return render_template('homepage_customer.html',
+                                   # same results as
+                                   departure_city=locations['departure_loc'],
+                                   arrival_city=locations['arrival_loc'],
+                                   all=data_dic,
+                                   purchased=purchased_flight,
+                                   flight_num=query.get_flight_num(conn),
+                                   total_month=total_month,
+                                   avg_month=avg_month)
+        else:
+            if html_get['customer_email'] is None or html_get['customer_email'] == '':
+                success, err = False, 'Please enter both flight num and the customer email.'
+                print('executing if_clause')
+            else:
+                print('executing else_clause')
+                print(html_get['customer_email'])
+                success, err = query.purchase(conn, flight_num, html_get['customer_email'], session['email'])
+            if success:
+                return redirect(url_for("agent_home"))
+            else:
+                return redirect(url_for("agent_home"))
 
 
 @app.route("/sign_in/staff_home", methods=["POST", "GET"])
@@ -293,21 +307,36 @@ def staff_home():
             'arrival_time': request.form.get('arrdate'),
             'departure': request.form.get('depplace'),
             'arrival': request.form.get('arrplace'),
-            'plane': request.form.get('plane')
+            'plane': request.form.get('plane'),
+            'status': request.form.get("status"),
+            'planeid': request.form.get("planeid"),
+            'seats' : request.form.get('seats')
         }
 
-        if query.create_flight(conn, session, create_para['flight_num'], create_para["price"], create_para["departure_time"],create_para['arrival_time'],create_para['departure'][-3:], create_para['arrival'][-3:], create_para['plane']):
-            return redirect(url_for('staff_home'))
-        else:
-            session['createflighterror'] = 'Create Flight failed'
-            return redirect(url_for("staff_home"))
+        if create_para['flight_num']:
+            if query.create_flight(conn, session, create_para['flight_num'], create_para["price"], create_para["departure_time"],create_para['arrival_time'],create_para['departure'][-3:], create_para['arrival'][-3:], create_para['plane']):
+                return redirect(url_for('staff_home'))
+            else:
+                session['createflighterror'] = 'Create Flight failed'
 
+        if create_para["status"]:
+            if query.change_flight_status(conn, create_para['flight_num'], create_para["status"]):
+                return redirect(url_for('staff_home'))
+            else:
+                session["createflighterror"] = 'Update Status failed'
 
+        if create_para["seats"]:
+            if query.add_airplane(conn,session, create_para["planeid"], create_para["seats"]):
+                session['createflighterror'] = 'Add airplane'
+            else:
+                session["createflighterror"] = 'Update Status failed'
+        return redirect(url_for("staff_home"))
 
 
 @app.route("/sign_out", methods = ['POST','GET'])
 def sign_out():
     if request.method == 'GET':
+        session.clear()
         return render_template('sign_out.html')
 
 
--- Query_Utility.py
+++ Query_Utility.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import pprint
 import random
 
-from werkzeug.security import generate_password_hash, check_password_hash
+# from werkzeug.security import generate_password_hash, check_password_hash
 
 PASSWORD_HASH = 'md5'
 # ===========================================================================================
@@ -125,18 +125,44 @@ def get_airline(conn, flight_num):
     return airline
 
 
-def purchase(conn, flight_num, customer_email, booking_agent_email = 'NULL'):
+def get_seats(conn, flight_num):
+    cursor = conn.cursor()
+    query = "select seats from airplane where airplane_id= " \
+            "(select airplane_id from flight where flight_num= \'%s\' );" % flight_num.strip()
+    print(query)
+    cursor.execute(query)
+    seats = cursor.fetchall()[0]['seats']
+    cursor.close()
+    print('airline got is:', seats)
+    return seats
+
+
+def ticket_sold(conn, flight_num):
+    cursor = conn.cursor()
+    query = "select COUNT(*) as ct from ticket" \
+            " where flight_num = \'%s\';" % flight_num
+    cursor.execute(query)
+    ticket_sold = cursor.fetchall()[0]['ct']
+    cursor.close()
+    print('airline got is:', ticket_sold)
+    return ticket_sold
+
+
+def purchase(conn, flight_num, customer_email, booking_agent_email):
     success, err = False, ''
-    # TODO!!! check whether the selected flight is full
-
-
+    # logic: the airline airplane the selected flight_num is using should <=
+    # the ticket num sold
+    seats = get_seats(conn, flight_num)
+    sold = ticket_sold(conn, flight_num)
+    if seats <= sold:
+        err = 'Sorry, the plane is currently full.'
+        return success, err
     # insert data into ticket （ticket_id, airline_name, flight_num）
     cursor = conn.cursor()
     ticket_id = random_ticket_id(conn)
     airline_name = get_airline(conn, flight_num)
     query = 'insert into ticket values' \
             '(\'%s\',\'%s\',\'%s\')' % (ticket_id,airline_name, flight_num)
-    print(query)
     cursor.execute(query)
     conn.commit()
     cursor.close()
@@ -145,19 +171,17 @@ def purchase(conn, flight_num, customer_email, booking_agent_email = 'NULL'):
     cursor = conn.cursor()
     year, month, day = getting_date()
     purchase_date = formatting_date(year, month, day)
-    print('formatted date',purchase_date)
     if booking_agent_email != 'Null':
         query = 'insert into purchases values' \
-            '(\'%s\',\'%s\',\'%s\',\'%s\')' % (ticket_id, customer_email, booking_agent_email,purchase_date)
+            '(\'%s\',\'%s\',\'%s\',\'%s\')' % (ticket_id, customer_email.strip(), booking_agent_email,purchase_date)
     else:
         query = 'insert into purchases values' \
-                '(\'%s\',\'%s\',%s,\'%s\')' % (ticket_id, customer_email, booking_agent_email, purchase_date)
+                '(\'%s\',\'%s\',%s,\'%s\')' % (ticket_id, customer_email.strip(), booking_agent_email, purchase_date)
     print(query)
     cursor.execute(query)
     conn.commit()
     cursor.close()
     success = True
-    print(success, err)
     return success, err
 
 # ======== Start of day time formatting fuction
@@ -175,7 +199,6 @@ def formatting_date(year,month,date):
     return str
 
 def getting_period(day):
-    print(type(day))
     start = '%s 00:00:00'%str(day)
     end = '%s 23:59:59'%str(day)
     return start, end
@@ -184,32 +207,12 @@ def getting_past_month_period(year,month,day):
     start = '%s 00:00:00'%(formatting_date(year,str(int(month)-1),str(int(day)+1)))
     end = '%s 23:59:59'%(formatting_date(year,month,day))
     return start, end
-# print(getting_past_month_period('2021','8','10'))
+
 
 def getting_past_year_period(year,month,day):
     start = '%s 00:00:00'%(formatting_date(str(int(year)-1),month,day))
     end = '%s 23:59:59'%(formatting_date(year,month,day))
     return start, end
-# print(getting_past_year_period(getting_date()))
-
-# ======== End of day time formatting fuction
-# the options for searching filter
-# NOT IN USE
-# def get_departure_time():
-#     d_dic = {
-#         'departure_time': [],
-#         'arrival_time': []
-#     }
-#
-#     for i in range(len(data)):
-#         d_dic['departure_time'].append(str(data[i]['departure_time']))
-#         d_dic['arrival_time'].append(str(data[i]['arrival_time']))
-#
-#     # get rid of all the duplicate elements
-#     d_dic['departure_time'] = remove_duplicate(d_dic['CHANGE'])
-#     d_dic['arrival_time'] = remove_duplicate(d_dic['CHANGE'])
-
-
 
 def public_view(conn):
     # From query fetch all
@@ -218,7 +221,6 @@ def public_view(conn):
     cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
-
     for i in data:
         i['Departure']= "%s | %s" % (airport_city(i['departure_airport']),i['departure_airport'])
         i['Arrival']= "%s | %s" % (airport_city(i['arrival_airport']),i['arrival_airport'])
@@ -232,14 +234,11 @@ def filter_result(conn,html_get):
     if html_get['to'] and html_get['to'] != None:
         html_get['arrival_airport'] = html_get['to'].split('|')[1].strip()
         query += ' and %s =\'%s\'' % ('arrival_airport', html_get['arrival_airport'])
-    if html_get['dt'] != '' and html_get['dt'] != None :
-        print(html_get['dt'])
+    if html_get['dt'] != '' and html_get['dt'] != None:
         start, end = getting_period(html_get['dt'])
-        print(start, end)
         query += ' and %s between \'%s\' and \'%s\'' % ('departure_time', start, end)
     if (html_get['from'] == '' or html_get['from'] is None) and (html_get['to']== '' or html_get['to'] is None) and (html_get['dt']== '' or html_get['dt'] is None):
         query += ' status = \'upcoming\''
-    print(query)
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
@@ -259,7 +258,6 @@ def get_purchased_flight(conn,session):
         query = query % ('booking_agent_email', session['email'])
     if session["user_type"] == 'airline_staff':
         query = 'select * from flight where flight_num in (select flight_num from ticket where airline_name = \'%s\')'%(session["airline_name"])
-    print(query)
     cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
@@ -284,7 +282,7 @@ def sign_in_check(conn, username, password,role, airline_name):
     cursor.close()
     if not data:
         return False
-    # return check_password_hash(data[0][0], password)
+    # return check_password_hash(data[0]['password'], password)
     return data[0]['password'] == password
 
 # ======== Sign up
@@ -293,12 +291,10 @@ def reg_validation_cus(conn,session):
     query = 'select * from customer ' \
             'where email = \'%s\' ' \
             'and password = \'%s\' '  % (session['email'], session['password'])
-    print(query)
-
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
-    print(data)
+    # print(data)
     cursor.close()
     if data:
         return False, 'Email already in use.'
@@ -309,11 +305,11 @@ def reg_validation_ba(conn,session):
             'where email = \'%s\' ' \
             'and password = \'%s\' ' \
             'and booking_agent_id = \'%s\''%(session['email'],session['password'],session['booking_agent_id'])
-    print(query)
+    # print(query)
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
-    print(data)
+    # print(data)
     cursor.close()
     if data:
         return  False,'Email already in use.'
@@ -322,7 +318,7 @@ def reg_validation_ba(conn,session):
 def reg_validation_as(conn,session):
     query = 'select * from airline_staff ' \
             'where username = \'%s\' ' \
-            'and password = \'%s\' '% (session['username'], session['password'])
+            'and password = \'%s\' '% (session['email'], session['password'])
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
@@ -360,7 +356,7 @@ def add_ba(conn, session):
 
 def add_as(conn, session):
     query = 'insert into airline_staff values' \
-            '(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')'%(session['username'],session['password'],
+            '(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\')'%(session['email'],session['password'],
                                                             session['first_name'],session['last_name'],
                                                             session['date_of_birth'],session['airline_name'],)
     cursor = conn.cursor()
@@ -406,12 +402,11 @@ def view_commission_month(conn, session):
     return total, avg, total1, avg1
 
 def get_top_customer_number(conn, session):
-    print(getting_past_month_period(getting_date()[0],getting_date()[1],getting_date()[2])[0],  getting_past_month_period(getting_date()[0],getting_date()[1],getting_date()[2])[1])
     query = 'SELECT customer_email, count(ticket_id) as tot_number FROM purchases ' \
             'WHERE booking_agent_email = \'%s\' AND purchase_date between \'%s\' and \'%s\' ' \
             'GROUP BY booking_agent_email ' \
             'ORDER BY tot_number DESC limit 5;' %(session['email'], getting_past_month_period(getting_date()[0], getting_date()[1], getting_date()[2])[0],  getting_past_month_period(getting_date()[0],getting_date()[1],getting_date()[2])[1])
-    print(query)
+    # print(query)
     cursor = conn.cursor()
     cursor.execute(query)
     data = cursor.fetchall()
@@ -419,7 +414,7 @@ def get_top_customer_number(conn, session):
     for i in range(len(data)):
         data_list.append([data[i]['customer_email'],data[i]['tot_number']])
     cursor.close()
-    print(data_list)
+    # print(data_list)
     return data_list
 
 
@@ -437,32 +432,41 @@ def get_top5_number(conn):
     for i in range(len(data)):
         data_list.append([data[i]['booking_agent_email'],data[i]['ct']])
     cursor.close()
-    print(data_list)
     return data_list
 
 def create_flight(conn, session, flight_num, price, departure_time, arrival_time,departure, arrival, plane):
-    success = False
-
     #insert into flight
     cursor = conn.cursor()
     query = 'insert into flight values' \
             '(\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'%s\',\'Upcoming\',\'%s\')' % (session['airline_name'], flight_num,  departure, arrival, departure_time, arrival_time, price, plane)
-    print(query)
     cursor.execute(query)
     conn.commit()
     cursor.close()
+    return True
 
-    success = True
-    return success
+def change_flight_status(conn, flight_num, status):
+    # insert into flight
+    cursor = conn.cursor()
+    query = 'UPDATE flight SET status = \'%s\' WHERE flight_num = \'%s\';' %(status, flight_num)
+    # print(query)
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    return True
 
-def change_flight_status(conn, session, flight_num):
-
+def add_airplane(conn, session, airplane_id, seats):
+    cursor = conn.cursor()
+    query = 'INSERT INTO airplane VALUES (\"%s\", \"%s\", \'%s\');' % (session['airline_name'], airplane_id, seats)
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+    return True
 
 
 # End of homepage utility function
 def check_full(dic):
     for key in dic.keys():
-        print(key, dic[key])
+        # print(key, dic[key])
         if dic[key] == '' or dic[key]== None:
             return False
     return True
